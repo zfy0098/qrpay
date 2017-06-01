722a2eed64e0587a3d54bd577a993af0191921ff
zfy0098 <zfy0098@163.com>
2017-06-01 14:25:18 +0800

    增加cache缓存

--- src/main/java/com/example/demo/controller/ClearCacheController.java
+++ src/main/java/com/example/demo/controller/ClearCacheController.java
@@ -1,8 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.constant.Constant;
-import com.example.demo.util.EhcacheUtil;
 import com.example.demo.util.LoggerTool;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +14,11 @@ public class ClearCacheController{
 	LoggerTool logger = new LoggerTool(this.getClass());
 	
 	@RequestMapping("")
+	@CacheEvict(value = Constant.cacheName, allEntries = true)
 	public Object ClearCache(){
-		logger.info("清除缓存中所有内容");
-		EhcacheUtil ehcache = EhcacheUtil.getInstance();
-		ehcache.clear(Constant.cacheName);
+//		logger.info("清除缓存中所有内容");
+//		EhcacheUtil ehcache = EhcacheUtil.getInstance();
+//		ehcache.clear(Constant.cacheName);
 		return "SUCCESS";
 	}
 }
